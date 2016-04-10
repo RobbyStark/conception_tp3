5e062d301aab33f359b4a9a2bc5f180ef1018312
Francis de Lasalle <francis.de-lasalle@polymtl.ca>
2016-04-09 22:59:45 -0400

    Server

---
+++ tp3_server/tp3_server/src/main/java/com/tp3_server/file_system/Component.java
@@ -0,0 +1,34 @@
+package com.tp3_server.file_system;
+
+/*
+ * Class that acts as an interface for the Composite and Leaf, represents a
+ * tree structure.
+ */
+public abstract class Component {
+	// Name of the component.
+	String path_;
+	
+	// Google Drive id.
+	String driveId_;
+		
+	/*
+	 * Returns the name of the component.
+	 */
+	public String getPath() {
+		return path_;
+	}
+	
+	/*
+	 * Returns the name of the component.
+	 */
+	public String getDriveId() {
+		return driveId_;
+	}
+	
+	/*
+	 * Adds a new component to the array of components.
+	 */
+	public void addComponent(Component c) {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ tp3_server/tp3_server/src/main/java/com/tp3_server/file_system/Leaf.java
@@ -0,0 +1,14 @@
+package com.tp3_server.file_system;
+
+/*
+ * Class that represents a leaf (file).
+ */
+public class Leaf extends Component {
+	/*
+	 * Class constructor.
+	 */
+	public Leaf(String path, String driveId) {
+		path_ = path;
+		driveId_ = driveId;
+	}
+}
